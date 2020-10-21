f6ff41d8604b7e5bb6fd212db93a2acb619dec9a
EmilianoArganaraz <emilianoarganaraz61@gmail.com>
2020-10-21 19:25:31 -0300

    second commit

--- lib/Application/Home/home.dart
+++ lib/Application/Home/home.dart
@@ -1,12 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:states_rebuilder_prueba/Domain/Home/Model/pelicula_model.dart';
-import 'package:states_rebuilder_prueba/Infrastructure/Home/home_repo.dart';
 
 class Home{
   int counter = 0;
   Home({this.counter});
 
-  HomeRepoSitory _homeRepo = HomeRepoSitory() ;
   List<Pelicula> listaPelicula;
 
   void incrementar(){
@@ -19,18 +17,6 @@ class Home{
 
   void inicializar(){
     counter = 0;
-  }
-
-
-  Future<void> getEnCines() async {
-    final Response<dynamic> response = await _homeRepo.fetchNowPlaying();
-
-    if (response.data['results'] != null) {
-      listaPelicula = response.data['results']
-          .map<Pelicula>((dynamic i) => Pelicula.fromJsonMap(i))
-          .toList();
-    }
-
   }
 
 }
